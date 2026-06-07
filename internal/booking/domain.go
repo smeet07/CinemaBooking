db5080ff709d4cfcb829d996a3807a6f0e3f97fb
Smeet Nagda <smeetnagda@Mac-239.lan>
2026-06-07 13:12:53 -0400

    in memory store implemenation

---
+++ internal/booking/domain.go
@@ -0,0 +1,18 @@
+package booking
+import "errors"
+
+var (
+	ErrSeatAlreadyBooked=errors.New("seat is already taken")
+)
+type Booking struct{
+	ID string
+	MovieID string
+	SeatID string
+	UserID string
+	Status string
+}
+
+type BookingStore interface{
+	Book(b Booking) error
+	ListBookings(movieID string) []Booking
+}
